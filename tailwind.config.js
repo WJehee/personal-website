05689f517ec9f40a7eba90128566714395337a1d
Wouter Jehee <wouter@wouterjehee.com>
2025-01-07 21:16:17 +0100

    bunch of changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,18 @@ module.exports = {
                         transform: 'rotate(-360deg)',
                     },
                 },
+                'line-lr': {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '100%',
+                    },
+                }
             },
             animation: {
                 'border-spin': 'border-spin 7s linear infinite',
+                'line-lr': 'line-lr 2s ease-out infinite',
             },
         },
     },
