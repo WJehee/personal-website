758973f95eaea7319489c2291a7613c47ec600ee
Wouter Jehee <wouter@wouterjehee.com>
2025-01-28 13:20:43 +0100

    add tailwind to flake build

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
     description = "Personal website v2";
-
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
-
     outputs = { self, nixpkgs }:
     let
         system = "x86_64-linux";
@@ -13,7 +11,10 @@
             pname = "Zola blog: wouterjehee.com";
             version = "1.0.0";
             src = ./.;
-            buildInputs = [ zola ];
+            buildInputs = [ 
+                zola
+                tailwindcss
+            ];
             buildPhase = ''
                 tailwindcss -i styles/styles.css -o static/css/styles.css --minify
                 zola build
